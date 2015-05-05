43a0e31e67717856bb46ffc94176c2f5323d3e63
David Negro Martín <d.negro@alumnos.upm.es>
2015-05-06 00:41:18 +0200

    Pereparando versión para subir a Heroku

--- models/models.js
+++ models/models.js
@@ -1,10 +1,25 @@
 var path = require('path');
+var url =process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name = (url[6]||null);
+var user    = (url[2]||null);
+var pwd     = (url[3]||null);
+var protocol= (url[1]||null);
+//var dialect = (url[1]||null);
+var port    = (url[5]||null);
+var host    = (url[4]||null);
+var storage = process.env.DATABASE_STORAGE;
 
 //Cargar Modelo ORM
 var Sequelize = require('sequelize');
 
 //Usar BBDD SQLite 
-var sequelize = new Sequelize(null,null,null, {dialect: "sqlite", storage: "quiz.sqlite"});
+var sequelize = new Sequelize(DB_name,user,pwd, 
+                              {dialect: protocol, 
+                               storage: protocol,
+                              port: port,
+                              host: host,
+                              storage: storage,
+                              omitNull: true});
 
 
 //Importar la definición de la tabla Quiz en quiz.js
@@ -15,7 +30,7 @@ exports.Quiz=Quiz; //exportar la definición de la tabla Quiz
 sequelize.sync().then(function(){
                          Quiz.count().success(function(count){
                                 if(count ===0){
-                                Quiz.create({pregunta: 'Capital Roma',
+                                Quiz.create({pregunta: 'Capital de Italia',
                                             respuesta: 'Roma'
                         }).then(function(){console.log('Base de datos inicializada');});
                         };
